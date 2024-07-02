6949d4e689f1c0b99e9fa09616b7d5b3415f1684
diego.gomez <diego.gomez@estudiantes.utec.edu.uy>
2024-07-02 15:04:46 -0300

    Se agregan endpoint para usuario

--- src/main/java/codigocreativo/uy/servidorapp/ws/UsuarioResource.java
+++ src/main/java/codigocreativo/uy/servidorapp/ws/UsuarioResource.java
@@ -1,6 +1,8 @@
 package codigocreativo.uy.servidorapp.ws;
 
+import codigocreativo.uy.servidorapp.DTO.EquipoDto;
 import codigocreativo.uy.servidorapp.DTO.UsuarioDto;
+import codigocreativo.uy.servidorapp.enumerados.Estados;
 import codigocreativo.uy.servidorapp.servicios.UsuarioRemote;
 import jakarta.ejb.EJB;
 import jakarta.ws.rs.*;
@@ -16,6 +18,12 @@ public class UsuarioResource {
     @EJB
     private UsuarioRemote er;
 
+    @GET
+    @Path("/Login")
+    public UsuarioDto loginUsuario(@QueryParam("usuario")  String usuario, @QueryParam("password") String password){
+        return this.er.login(usuario, password);
+    }
+
     @POST
     @Path("")
     public Response crearEquipo(UsuarioDto usuario) {
@@ -30,11 +38,34 @@ public class UsuarioResource {
         return Response.status(200).build();
     }
 
+    @PUT
+    @Path("Inactivar")
+    public Response inactivarUsuario(UsuarioDto usuario){
+        this.er.eliminarUsuario(usuario);
+        return Response.status(200).build();
+    }
+
+    @GET
+    @Path("/BuscarUsuarioPorCI")
+    public UsuarioDto buscarEquipo(@QueryParam("ci") String ci){
+        return this.er.obtenerUsuarioPorCI(ci);
+    }
+
+    @GET
+    @Path("/BuscarUsuarioPorId")
+    public UsuarioDto buscarEquipo(@QueryParam("id") Long id){
+        return this.er.obtenerUsuario(id);
+    }
+
+    @GET
+    @Path("/ObtenerUsuarioPorEstado")
+    public List<UsuarioDto> obtenerUsuarioPorEstado(@QueryParam("estado") Estados estado){
+        return this.er.obtenerUsuariosPorEstado(estado);
+    }
 
     @GET
     @Path("/ListarTodosLosUsuarios")
     public List<UsuarioDto> obtenerTodosLosUsuarios(){
         return this.er.obtenerUsuarios();
     }
-
 }
