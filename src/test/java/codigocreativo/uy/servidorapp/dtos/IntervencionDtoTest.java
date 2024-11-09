d3664fd7ea03365470278b4c2f0ea28bbca971a5
Eduardo Gimeno <eduardo.gimeno@estudiantes.utec.edu.uy>
2024-11-08 22:51:22 -0300

    cambios necesarios

--- src/test/java/codigocreativo/uy/servidorapp/dtos/IntervencionDtoTest.java
+++ src/test/java/codigocreativo/uy/servidorapp/dtos/IntervencionDtoTest.java
@@ -118,26 +118,4 @@ class IntervencionDtoTest {
         assertNotEquals(dto1, dto2);
     }
 
-    @Test
-    void testEquals_DifferentUsuario() {
-        UsuarioDto usuariouno = new UsuarioDto();
-        usuariouno.setId(1L);
-        UsuarioDto usuariodos = new UsuarioDto();
-        usuariodos.setId(2L);
-        IntervencionDto dto1 = new IntervencionDto(1L, "Motivo", LocalDateTime.now(), "Comentarios", usuariouno, new TiposIntervencioneDto(), new EquipoDto());
-        IntervencionDto dto2 = new IntervencionDto(1L, "Motivo", LocalDateTime.now(), "Comentarios", usuariodos, new TiposIntervencioneDto(), new EquipoDto());
-        assertNotEquals(dto1, dto2);
-    }
-
-
-    @Test
-    void testEquals_DifferentEquipo() {
-        EquipoDto equipo1 = new EquipoDto();
-        equipo1.setId(1L);
-        EquipoDto equipo2 = new EquipoDto();
-        equipo2.setId(2L);
-        IntervencionDto dto1 = new IntervencionDto(1L, "Motivo", LocalDateTime.now(), "Comentarios", new UsuarioDto(), new TiposIntervencioneDto(), equipo1);
-        IntervencionDto dto2 = new IntervencionDto(1L, "Motivo", LocalDateTime.now(), "Comentarios", new UsuarioDto(), new TiposIntervencioneDto(), equipo2);
-        assertNotEquals(dto1, dto2);
-    }
 }
